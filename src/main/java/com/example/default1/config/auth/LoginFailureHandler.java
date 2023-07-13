695d1d33b93d9551d9d20663f3edefde0f95b83d
toyriding <toyriding@trgtech.co.kr>
2023-07-13 13:26:32 +0900

    Well

--- src/main/java/com/example/default1/config/auth/LoginFailureHandler.java
+++ src/main/java/com/example/default1/config/auth/LoginFailureHandler.java
@@ -1,18 +1,19 @@
 package com.example.default1.config.auth;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.default1.base.model.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+@Slf4j
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
     @Override
@@ -20,13 +21,9 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
                                         AuthenticationException exception) throws IOException {
 
         if (request.getHeader("x-requested-with") != null && request.getHeader("x-requested-with").equals("XMLHttpRequest")) {
-            Map<String, Object> map = Map.of(
-                "isLogin", false
-                , "message", exception.getMessage()
-            );
             MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
             MediaType jsonMimeType = MediaType.APPLICATION_JSON;
-            jsonConverter.write(map, jsonMimeType, new ServletServerHttpResponse(response));
+            jsonConverter.write(Response.fail(2, exception.getMessage()), jsonMimeType, new ServletServerHttpResponse(response));
         } else {
             response.sendRedirect("/login?error=2");
         }
--- src/main/java/com/example/default1/config/auth/LoginSuccessHandler.java
+++ src/main/java/com/example/default1/config/auth/LoginSuccessHandler.java
@@ -1,11 +1,11 @@
 package com.example.default1.config.auth;
 
 import java.io.IOException;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.default1.base.model.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServletServerHttpResponse;
@@ -18,13 +18,9 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException {
 
         if (request.getHeader("x-requested-with") != null && request.getHeader("x-requested-with").equals("XMLHttpRequest")) {
-            Map<String, Object> map = Map.of(
-                "isLogin", true,
-                "message", "로그인되었습니다."
-            );
             MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
             MediaType jsonMimeType = MediaType.APPLICATION_JSON;
-            jsonConverter.write(map, jsonMimeType, new ServletServerHttpResponse(response));
+            jsonConverter.write(Response.success("로그인되었습니다."), jsonMimeType, new ServletServerHttpResponse(response));
         } else {
             response.sendRedirect("/");
         }
