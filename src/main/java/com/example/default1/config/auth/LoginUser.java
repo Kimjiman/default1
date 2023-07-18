37bcff476707b094867491439660c7f820f8c18d
toyriding <toyriding@trgtech.co.kr>
2023-07-18 11:21:53 +0900

    Fix

--- src/main/java/com/example/default1/config/auth/LoginUser.java
+++ src/main/java/com/example/default1/config/auth/LoginUser.java
@@ -1,7 +1,6 @@
 package com.example.default1.config.auth;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +11,6 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoginUser {
     private Long id;
