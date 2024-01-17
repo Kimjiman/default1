3c054ba12021602e68e11ef9a3c4418526cbbc79
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-17 13:47:13 +0900

    [Fix] Pager 수정

--- src/main/java/com/example/default1/utils/Pager.java
+++ src/main/java/com/example/default1/utils/Pager.java
@@ -107,10 +107,10 @@ public class Pager {
 
     /**
      * 글의 번호 매기기
-     * @param objectList ObjectList extends Pager
+     * @param objectList ? extends Pager
      */
     public void rowNumberOver(Pager pager, List<? extends Pager> objectList) {
-        for(int i = pager.getTotalRow() - 1; i >= 0; i--) {
+        for(int i = objectList.size() - 1; i >= 0; i--) {
             objectList.get(i).setRowNum(pager.getTotalRow() - ((pager.getPage() - 1) * pager.getLimit() + i));
         }
     }
