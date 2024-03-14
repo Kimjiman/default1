ae60ebaee92e2a0ad72475d6270f88ebeee0edfb
DESKTOP-6JBTTB2\interweb <kjmmaster@naver.com>
2024-03-14 13:52:27 +0900

    [Feature] 유틸리티변경

--- src/main/java/com/example/default1/utils/DateUtils.java
+++ src/main/java/com/example/default1/utils/DateUtils.java
@@ -7,78 +7,66 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateUtils {
-    private static final String DATE_FORMAT = "yyyy.MM.dd";
-    private static final String DATETIME_FORMAT = "yyyy.MM.dd HH:mm:ss";
-    private static final String TIME_FORMAT = "HH:mm:ss";
+    private static final String DATE_PATTERN = "yyyy.MM.dd";
+    private static final String DATETIME_PATTERN = "yyyy.MM.dd HH:mm:ss";
+    private static final String TIME_PATTERN = "HH:mm:ss";
 
-    public static Date toDate(LocalDateTime date) {
-        return Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+    public static boolean isValidDateFormat(String dateString, String pattern) {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+            formatter.setLenient(false);
+            formatter.parse(dateString);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
     }
 
-    public static LocalDateTime toLocalDateTime(Date date) {
-        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-    }
-
-    public static Date stringToDateTime(String dateString) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat(DATETIME_FORMAT);
+    public static Date stringToDate(String dateString, String pattern) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
         return formatter.parse(dateString);
     }
 
-    public static LocalDateTime stringToLocalDateTime(String dateString) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+    public static Date localDateTimeToDate(LocalDateTime date) {
+        return Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    public static LocalDateTime stringToLocalDateTime(String dateString, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return LocalDateTime.parse(dateString, formatter);
     }
 
-    public static String now() {
-        return getDate(LocalDateTime.now());
+    public static String toDateString(LocalDateTime date, String pattern) {
+        return date.format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    public static String getDate(Date date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
+    public static String toDateString(LocalDate date, String pattern) {
+        return date.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static String toDateString(Date date, String pattern) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         return simpleDateFormat.format(date);
     }
 
-    public static String getDate(LocalDateTime date) {
-        return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    public static String getDateString(LocalDateTime date) {
+        return toDateString(date, DATE_PATTERN);
     }
 
-    public static String getDate(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    public static String getDateTimeString(LocalDateTime date) {
+        return toDateString(date, DATETIME_PATTERN);
     }
 
-    public static String getDatetime(Date date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATETIME_FORMAT);
-        return simpleDateFormat.format(date);
-    }
-
-    public static String getDatetime(LocalDateTime date) {
-        return date.format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
-    }
-
-    public static String getDatetime(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
-    }
-
-    public static String getTime(Date date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(TIME_FORMAT);
-        return simpleDateFormat.format(date);
-    }
-
-    public static String getTime(LocalDateTime date) {
-        return date.format(DateTimeFormatter.ofPattern(TIME_FORMAT));
-    }
-
-    public static String getTime(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern(TIME_FORMAT));
-    }
-
-    public static Integer getDayOfWeekNumber(Date date) {
-        LocalDateTime date2 = toLocalDateTime(date);
-        DayOfWeek dayOfWeek = date2.getDayOfWeek();
-        return dayOfWeek.getValue();
+    public static String getTimeString(LocalDateTime date) {
+        return toDateString(date, TIME_PATTERN);
     }
 
     public static Integer getDayOfWeekNumber(LocalDateTime date) {
@@ -86,32 +74,37 @@ public class DateUtils {
         return dayOfWeek.getValue();
     }
 
-    public static Integer getDayOfWeekNumber(LocalDate date) {
+    public static String getDayOfWeekString(LocalDateTime date) {
         DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return dayOfWeek.getValue();
-    }
-
-    public static int compareTo(Date date, Date date2) {
-        return date.compareTo(date2);
+        switch (dayOfWeek) {
+            case MONDAY:
+                return "Monday";
+            case TUESDAY:
+                return "Tuesday";
+            case WEDNESDAY:
+                return "Wednesday";
+            case THURSDAY:
+                return "Thursday";
+            case FRIDAY:
+                return "Friday";
+            case SATURDAY:
+                return "Saturday";
+            case SUNDAY:
+                return "Sunday";
+            default:
+                return "";
+        }
     }
 
     public static int compareTo(LocalDateTime date, LocalDateTime date2) {
         return date.compareTo(date2);
     }
 
-    public static int compareTo(LocalDate date, LocalDate date2) {
-        return date.compareTo(date2);
+    public static long daysBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return ChronoUnit.DAYS.between(startDate, endDate);
     }
 
-    public static boolean betweenTo(Date date, Date date2, Date targetDate) {
-        Date startDate = (date.before(date2)) ? date : date2;
-        Date endDate = (date.after(date2)) ? date : date2;
-
-        return (targetDate.equals(startDate) || targetDate.after(startDate)) &&
-                (targetDate.equals(endDate) || targetDate.before(endDate));
-    }
-
-    public static boolean betweenTo(LocalDateTime date, LocalDateTime date2, LocalDateTime targetDate) {
+    public static boolean isBetween(LocalDateTime date, LocalDateTime date2, LocalDateTime targetDate) {
         LocalDateTime startDate = (date.isBefore(date2)) ? date : date2;
         LocalDateTime endDate = (date.isAfter(date2)) ? date : date2;
 
@@ -119,11 +112,43 @@ public class DateUtils {
                 (targetDate.equals(endDate) || targetDate.isBefore(endDate));
     }
 
-    public static boolean betweenTo(LocalDate date, LocalDate date2, LocalDate targetDate) {
-        LocalDate startDate = (date.isBefore(date2)) ? date : date2;
-        LocalDate endDate = (date.isAfter(date2)) ? date : date2;
+    public static String getDateString(Date date) {
+        return toDateString(date, DATE_PATTERN);
+    }
 
-        return (targetDate.equals(startDate) || targetDate.isAfter(startDate)) &&
-                (targetDate.equals(endDate) || targetDate.isBefore(endDate));
+    public static String getDateTimeString(Date date) {
+        return toDateString(date, DATETIME_PATTERN);
+    }
+
+    public static String getTimeString(Date date) {
+        return toDateString(date, TIME_PATTERN);
+    }
+
+    public static Integer getDayOfWeekNumber(Date date) {
+        LocalDateTime date2 = dateToLocalDateTime(date);
+        return getDayOfWeekNumber(date2);
+    }
+
+    public static String getDayOfWeekString(Date date) {
+        LocalDateTime date2 = dateToLocalDateTime(date);
+        return getDayOfWeekString(date2);
+    }
+
+    public static int compareTo(Date date, Date date2) {
+        return date.compareTo(date2);
+    }
+
+    public static long daysBetween(Date startDate, Date endDate) {
+        LocalDateTime start = dateToLocalDateTime(startDate);
+        LocalDateTime end = dateToLocalDateTime(endDate);
+        return daysBetween(start, end);
+    }
+
+    public static boolean isBetween(Date date, Date date2, Date targetDate) {
+        Date startDate = (date.before(date2)) ? date : date2;
+        Date endDate = (date.after(date2)) ? date : date2;
+
+        return (targetDate.equals(startDate) || targetDate.after(startDate)) &&
+                (targetDate.equals(endDate) || targetDate.before(endDate));
     }
 }
