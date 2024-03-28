ea026a5f89b0551d29a9caeb097736c163fa715e
DESKTOP-6JBTTB2\interweb <kjmmaster@naver.com>
2024-03-28 18:11:57 +0900

    [Fix]

--- src/main/java/com/example/default1/utils/UrlUtils.java
+++ src/main/java/com/example/default1/utils/UrlUtils.java
@@ -4,9 +4,22 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class UrlUtils {
-    public static String currentDomain() {
+    public static String getFullDomain() {
+        return getScheme() + "://" + getServerName() + ":" + getServerPort();
+    }
+
+    public static String getScheme() {
         ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-        return attr.getRequest().getScheme() + "://" + attr.getRequest().getServerName() +
-                ":" + attr.getRequest().getServerPort();
+        return attr.getRequest().getScheme();
+    }
+
+    public static String getServerName() {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return attr.getRequest().getServerName();
+    }
+
+    public static int getServerPort() {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return attr.getRequest().getServerPort();
     }
 }
