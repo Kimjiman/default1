baa1d00f93402fd3dce83bba9b7b6808df690fe2
JIMAN\kjmma <kjmmaster@naver.com>
2025-03-03 10:50:56 +0900

    [Fix]

---
+++ src/main/java/com/example/default1/component/SocketComponent.java
@@ -0,0 +1,14 @@
+package com.example.default1.component;
+
+/**
+ * packageName    : com.example.default1.component
+ * fileName       : SocketComponent
+ * author         : KIM JIMAN
+ * date           : 25. 3. 3. 월요일
+ * description    :
+ * ===========================================================
+ * DATE           AUTHOR          NOTE
+ * -----------------------------------------------------------
+ * 25. 3. 3.     KIM JIMAN      First Commit
+ */public class SocketComponent {
+}
