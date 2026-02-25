0a122c52f83b59dbb2df64c7b556112c689f47ea
JIMAN\kjmma <kjmmaster@naver.com>
2026-02-25 23:10:05 +0900

    [Fix] RedisConfig에서 RedisStandaloneConfiguration으로 변경 및 불필요한 import 제거

--- src/main/java/com/example/basicarch/config/RedisConfig.java
+++ src/main/java/com/example/basicarch/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -23,11 +24,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
         if (redisPassword != null && !redisPassword.isBlank()) {
-            factory.setPassword(redisPassword);
+            config.setPassword(redisPassword);
         }
-        return factory;
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
